68405813b1a63f3646ed0179f0b45152c650041e
Omulosi <mulongojohnpaul@gmail.com>
2019-11-21 12:36:00 +0300

    Remove endpoints for updating comments directly

--- src/routes/comments/index.js
+++ src/routes/comments/index.js
@@ -6,7 +6,5 @@ const schemas = require('../../validators/schema');
 
 router.get('/', comments.getAllComments);
 router.get('/:id', comments.getComment);
-router.put('/:id', restricted, validator(schemas.comment, 'body'), comments.updateComment);
-router.delete('/:id', restricted, comments.deleteComment);
 
 module.exports = router;
