a53162785df2cee3b4f06002e96df4d84f16cfd6
Omulosi <mulongojohnpaul@gmail.com>
2019-11-21 11:58:15 +0300

    Update stories controller
    Add the following functionality:
    * Ability to add/edit/delete a comment to a given story
    * Ability to view all comments for a given story

--- src/controllers/stories.controller.js
+++ src/controllers/stories.controller.js
@@ -11,26 +11,33 @@ const getStory = (req, res) => {
   const { id } = req.params;
 
   Items.findById(id)
-    .then((item) => {
+    .then(async (item) => {
       if (item.type === 'story') {
+        const comments = await Items.findBy({ type: 'comment', parent: id });
+        console.log('######################## ', item.comments.length);
         res.status(200).json({ status: 200, data: [item] });
       } else {
         res.status(404).json({ status: 404, error: 'Story not found' });
       }
     })
     .catch((error) => {
-      res.status(500).json({ status: 500, error: `Error getting project actions: ${error.message}` });
+      res.status(500).json({ status: 500, error: `Error getting story: ${error.message}` });
     });
 };
 
 const addStory = (req, res) => {
-  const story = req.body;
-  Items.add(story)
-    .then((newStory) => {
-      res.status(201).json({ status: 200, data: [{ ...newStory }] });
+  const { title, url } = req.body;
+  const { username } = req.decodedToken;
+  const newStory = {
+    title, url, by: username, type: 'story',
+  };
+
+  Items.add(newStory)
+    .then((story) => {
+      res.status(201).json({ status: 200, data: story });
     })
     .catch((error) => {
-      res.status(500).json({ status: 500, error: `Error adding comment: ${error.message}` });
+      res.status(500).json({ status: 500, error: `Error adding story: ${error}` });
     });
 };
 
@@ -44,7 +51,7 @@ const deleteStory = (req, res) => {
     .catch((error) => {
       res.status(500).json({
         status: 500,
-        error: `Error deleting comment: ${error.message}`,
+        error: `Error deleting story: ${error.message}`,
       });
     });
 };
@@ -60,10 +67,102 @@ const updateStory = (req, res) => {
     .catch((error) => {
       res.status(500).json({
         status: 500,
-        error: `Error updating the project: ${error.message}`,
+        error: `Error updating story: ${error.message}`,
       });
     });
 };
+
+const addCommentToStory = (req, res) => {
+  const comment = req.body;
+  const { id } = req.params;
+
+  Items.findById(id)
+    .then(async (item) => {
+      if (item.type === 'story') {
+        comment.parent = Number(id);
+        comment.type = 'comment';
+        const commentId = await Items.addComment(comment, id);
+        res.status(200).json({ status: 200, data: [{ id: commentId, message: 'comment successfully added' }] });
+      } else {
+        res.status(404).json({ status: 404, error: 'The story does not exist' });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ status: 500, error: `Error: ${error.message}` });
+    });
+};
+
+const getAllStoryComments = (req, res) => {
+  const { id } = req.params;
+  Items.findBy({ type: 'comment', parent: id })
+    .then((comments) => {
+      if (comments) {
+        res.status(200).json({ status: 200, data: comments });
+      } else {
+        res.status(404).json({ status: 404, message: 'No comments for this story' });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ status: 500, error: `Error getting comments from db: ${error.message}` });
+    });
+};
+
+const getStoryComment = (req, res) => {
+  const { storyId, commentId } = req.params;
+
+  Items.findBy({ type: 'comment', parent: storyId, id: commentId })
+    .then((comment) => {
+      if (comment) {
+        res.status(200).json({ status: 200, data: comment });
+      } else {
+        res.status(404).json({ status: 404, message: 'Comment not found' });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ status: 500, error: `Error getting comment from db: ${error.message}` });
+    });
+};
+
+
+const updateStoryComment = (req, res) => {
+  const { commentId } = req.params;
+  const updatedComment = req.body;
+
+  Items.update(updatedComment, commentId)
+    .then(async () => {
+      const comment = await Items.findById(Number(commentId));
+      res.status(200).json({ status: 200, data: comment });
+    })
+    .catch((error) => {
+      res.status(500).json({ status: 500, error: `Error getting comment from db: ${error.message}` });
+    });
+};
+
+
+const deleteStoryComment = (req, res) => {
+  const { storyId, commentId } = req.params;
+
+  Items.findBy({ type: 'comment', parent: storyId, id: commentId })
+    .then(async (comment) => {
+      if (comment) {
+        try {
+          await Items.removeComment(commentId);
+          res.status(200).json({ status: 200, data: [{ message: 'Comment has been deleted', commentId }] });
+        } catch (err) {
+          res.status(500).json({
+            status: 500,
+            error: `Error deleting story: ${err.message}`,
+          });
+        }
+      } else {
+        res.status(404).json({ status: 404, message: 'Comment not found' });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ status: 500, error: `Error getting comment from db: ${error.message}` });
+    });
+};
+
 
 module.exports = {
   getAllStories,
@@ -71,4 +170,9 @@ module.exports = {
   getStory,
   updateStory,
   deleteStory,
+  addCommentToStory,
+  getAllStoryComments,
+  getStoryComment,
+  updateStoryComment,
+  deleteStoryComment,
 };
