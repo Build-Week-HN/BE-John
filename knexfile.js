10d4446134ddc7775d3d6596bbad601fb1ffe200
Omulosi <mulongojohnpaul@gmail.com>
2019-11-21 14:38:56 +0300

    deploy updates

--- knexfile.js
+++ knexfile.js
@@ -35,16 +35,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './src/db/migrations',
     },
